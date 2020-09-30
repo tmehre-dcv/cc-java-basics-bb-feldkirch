4d18acf2b4913d85f29923c0e40e736e4936f16a
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2020-09-30 18:00:40 +0200

    My first Change

--- 01303_monthlyresults/src/MonthlyResults.java
+++ 01303_monthlyresults/src/MonthlyResults.java
@@ -1,3 +1,18 @@
+/*
+Ein Buchladen hat den folgenden Montastergebnisse:
+int[] businessResult = {-1, 10, 2, 5, -4, -3, -2, 0};
+
+
+_ Was ist das Gesamtergebnis? (Summe der Monatsergebnisse)
+_ Was ist das beste Monatsergebnis?
+_ In welchem Monat war das beste Monatsergebnis?
+_ Was ist das schwächste Monatsergebnis?
+_ In welchem Monat war das schwächste Monatsergebnis?
+_ War zwei hintereinander kommende Monate mit negativen Ergebnis?
+_ Was war das längste Intervall mit negativen Ergebnis?
+_ Was war das zweitbeste Ergebnis?
+
+ */
 public class MonthlyResults {
     public static void main(String[] args) {
         int[] businessResult = {-1, 10, 2, 5, -4, -3, -2, 0};
