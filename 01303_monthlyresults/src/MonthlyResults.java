02e487fc81764fb84d2f55953f3660d60c7d4615
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2020-10-02 10:24:42 +0200

    Implemented all cases

--- 01303_monthlyresults/src/MonthlyResults.java
+++ 01303_monthlyresults/src/MonthlyResults.java
@@ -16,5 +16,70 @@ _ Was war das zweitbeste Ergebnis?
 public class MonthlyResults {
     public static void main(String[] args) {
         int[] businessResult = {-1, 10, 2, 5, -4, -3, -2, 0};
+
+        int totalSum=0;
+        int maxValue=businessResult[0];
+        int maxValIdx=0;
+        int minValue=businessResult[0];
+        int minValIdx=0;
+
+        int negativeFigureIntervalCount=0;
+        int maxNegativeFigureIntervalCount=0;
+
+        for (int i=0; i < businessResult.length;i++) {
+            // Summieren
+            totalSum += businessResult[i];
+
+            // negativ?
+            if(businessResult[i]<0){
+                negativeFigureIntervalCount++;
+            }else{
+                // aktuelles Maximum merken
+                if(negativeFigureIntervalCount>= maxNegativeFigureIntervalCount) {
+                    maxNegativeFigureIntervalCount = negativeFigureIntervalCount;
+                }
+
+                // da nicht mehr negativ und damit das Intervall endet, muss Zähler auf 0 gesetzt werden!
+                negativeFigureIntervalCount=0;
+            }
+
+            // größer als aktuelles Maxium?
+            if (businessResult[i] >= maxValue) {
+                maxValue = businessResult[i];
+                maxValIdx = i+1;
+            }
+
+            // kleiner oder gleich als aktuelles Minimum?
+            if (businessResult[i] <= minValue) {
+                minValue = businessResult[i];
+                minValIdx = i+1;
+            }
+        }
+
+        /*
+        Wir ermitteln das zweitbeste Ergebnis separat, um die Komplexität der oberen Schleife gering zu halten.
+        Könnten wir das Ergebnis auch innerhalb der oberen Schleife ermitteln?
+        Welche Herausforderungen könnte die kombinierte Lösung mit sich bringen?
+         */
+        int secondBest = businessResult[0];
+        for(int i=0; i < businessResult.length; i++){
+
+            boolean greaterOrEqualCurrentSecondBest = businessResult[i] >= secondBest;
+            boolean lessThanMaxValue = businessResult[i] < maxValue;
+
+            // größer oder gleich bisher zweitbestes Ergebnis und kleiner als bestes Ergebnis
+            if(greaterOrEqualCurrentSecondBest && lessThanMaxValue){
+                secondBest =businessResult[i];
+            }
+        }
+
+        System.out.println("Summe der Ergebnisse: " + totalSum);
+        System.out.println("Bestes Monatsergebnis: " + maxValue);
+        System.out.println("Monat mit bestem Ergebnis: " + maxValIdx);
+        System.out.println("Schlechtestes Ergebnis: " + minValue);
+        System.out.println("Monat mit schlechtestem Ergebnis: " + minValIdx);
+        System.out.println("War zwei hintereinander kommende Monate mit negativen Ergebnis? " + minValIdx);
+        System.out.println("Was war das längste Intervall mit negativen Ergebnis? " + maxNegativeFigureIntervalCount);
+        System.out.println("Zweitbestes Ergebnis: " + secondBest);
     }
 }
